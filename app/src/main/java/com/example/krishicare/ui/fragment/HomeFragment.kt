5b6d44505a68bc3bcf13cd1e68877cbabda9f6fa
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-04-13 16:41:16 +0530

    added all features

--- app/src/main/java/com/example/krishicare/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/krishicare/ui/fragment/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.krishicare.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.KrishiCare.utils.Constants.CHAT_BOT
 import com.example.krishicare.R
 import com.example.krishicare.databinding.HomeFragmentBinding
 import com.example.krishicare.ui.activites.BaseFragments
@@ -36,6 +39,19 @@ class HomeFragment : BaseFragments() {
         binding.btnfertilizer.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_fertilizerFragment2)
         }
+        binding.btnChat.setOnClickListener {
+            openUrl()
+        }
 
+        binding.btnhmgrdn.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_agroBlogFragment)
+        }
+
+    }
+
+    private fun openUrl() {
+        val uri = Uri.parse(CHAT_BOT)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
     }
 }
